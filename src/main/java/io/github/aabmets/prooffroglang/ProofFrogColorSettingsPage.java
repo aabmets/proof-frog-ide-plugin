f1030f7fdc0e0bee99133be50fb8ea7043418f1b
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-05-03 09:35:32 +0300

    Added syntax highlighter and color settings page

---
+++ src/main/java/io/github/aabmets/prooffroglang/ProofFrogColorSettingsPage.java
@@ -0,0 +1,66 @@
+package io.github.aabmets.prooffroglang;
+
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.options.colors.AttributesDescriptor;
+import com.intellij.openapi.options.colors.ColorDescriptor;
+import com.intellij.openapi.options.colors.ColorSettingsPage;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class ProofFrogColorSettingsPage implements ColorSettingsPage {
+
+    private static final AttributesDescriptor[] DESCRIPTORS = new AttributesDescriptor[] {
+        new AttributesDescriptor("Keywords", ProofFrogSyntaxHighlighter.KEYWORD),
+        new AttributesDescriptor("String literal", ProofFrogSyntaxHighlighter.STRING),
+        new AttributesDescriptor("Line comment", ProofFrogSyntaxHighlighter.COMMENT),
+        new AttributesDescriptor("Semicolon", ProofFrogSyntaxHighlighter.SEMICOLON),
+        new AttributesDescriptor("Bad character", ProofFrogSyntaxHighlighter.BAD_CHARACTER),
+    };
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "ProofFrog";
+    }
+
+    @Override
+    public Icon getIcon() {
+        return ProofFrogIcons.FILE;
+    }
+
+    @Override
+    public AttributesDescriptor @NotNull [] getAttributeDescriptors() {
+        return DESCRIPTORS;
+    }
+
+    @Override
+    public ColorDescriptor @NotNull [] getColorDescriptors() {
+        return ColorDescriptor.EMPTY_ARRAY;
+    }
+
+    @NotNull
+    @Override
+    public SyntaxHighlighter getHighlighter() {
+        return new ProofFrogSyntaxHighlighter();
+    }
+
+    @NotNull
+    @Override
+    public Map<String, TextAttributesKey> getAdditionalHighlightingTagToDescriptorMap() {
+        return Map.of();
+    }
+
+    @NotNull
+    @Override
+    public String getDemoText() {
+        return """
+            // Example ProofFrog code
+            import 'EncDec.game' as Game;
+            // Load a module
+            """;
+    }
+
+}
---
+++ src/main/java/io/github/aabmets/prooffroglang/ProofFrogSyntaxHighlighter.java
@@ -0,0 +1,80 @@
+package io.github.aabmets.prooffroglang;
+
+import io.github.aabmets.prooffroglang.psi.ProofFrogTypes;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.TokenSet;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+
+public class ProofFrogSyntaxHighlighter extends SyntaxHighlighterBase {
+
+    public static final TextAttributesKey KEYWORD =
+        TextAttributesKey.createTextAttributesKey(
+            "PROOFFROG_KEYWORD",
+            DefaultLanguageHighlighterColors.KEYWORD
+        );
+
+    public static final TextAttributesKey STRING =
+        TextAttributesKey.createTextAttributesKey(
+            "PROOFFROG_STRING",
+            DefaultLanguageHighlighterColors.STRING
+        );
+
+    public static final TextAttributesKey COMMENT =
+        TextAttributesKey.createTextAttributesKey(
+            "PROOFFROG_COMMENT",
+            DefaultLanguageHighlighterColors.LINE_COMMENT
+        );
+
+    public static final TextAttributesKey SEMICOLON =
+        TextAttributesKey.createTextAttributesKey(
+            "PROOFFROG_SEMICOLON",
+            DefaultLanguageHighlighterColors.SEMICOLON
+        );
+
+    public static final TextAttributesKey BAD_CHARACTER =
+        TextAttributesKey.createTextAttributesKey(
+            "PROOFFROG_BAD_CHARACTER",
+            HighlighterColors.BAD_CHARACTER
+        );
+
+    private static final TokenSet KEYWORD_TOKENS = TokenSet.create(
+            ProofFrogTypes.IMPORT,
+            ProofFrogTypes.AS
+    );
+
+    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
+    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
+    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+    private static final TextAttributesKey[] SEMICOLON_KEYS = new TextAttributesKey[]{SEMICOLON};
+    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
+    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+
+    @Override
+    public @NotNull Lexer getHighlightingLexer() {
+        return new ProofFrogLexerAdapter();
+    }
+
+    @Override
+    public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
+        if (KEYWORD_TOKENS.contains(tokenType)) {
+            return KEYWORD_KEYS;
+        } else if (tokenType.equals(ProofFrogTypes.FILESTRING)) {
+            return STRING_KEYS;
+        } else if (tokenType.equals(ProofFrogTypes.LINE_COMMENT)) {
+            return COMMENT_KEYS;
+        } else if (tokenType.equals(ProofFrogTypes.SEMI)) {
+            return SEMICOLON_KEYS;
+        } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
+            return BAD_CHAR_KEYS;
+        } else {
+            return EMPTY_KEYS;
+        }
+    }
+}
