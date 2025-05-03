63a9ad1fe44d8ca2d6e2a43cfd9a3817129c9d2a
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-05-03 17:09:05 +0300

    Updated autogenerated files

--- src/main/gen/io/github/aabmets/prooffroglang/parser/ProofFrogParser.java
+++ src/main/gen/io/github/aabmets/prooffroglang/parser/ProofFrogParser.java
@@ -36,23 +36,6 @@ public class ProofFrogParser implements PsiParser, LightPsiParser {
   }
 
   /* ********************************************************** */
-  // SET|BOOL|VOID|INT|MAP|BITSTRING|ARRAY
-  public static boolean datatype(PsiBuilder b, int l) {
-    if (!recursion_guard_(b, l, "datatype")) return false;
-    boolean r;
-    Marker m = enter_section_(b, l, _NONE_, DATATYPE, "<datatype>");
-    r = consumeToken(b, SET);
-    if (!r) r = consumeToken(b, BOOL);
-    if (!r) r = consumeToken(b, VOID);
-    if (!r) r = consumeToken(b, INT);
-    if (!r) r = consumeToken(b, MAP);
-    if (!r) r = consumeToken(b, BITSTRING);
-    if (!r) r = consumeToken(b, ARRAY);
-    exit_section_(b, l, m, r, false, null);
-    return r;
-  }
-
-  /* ********************************************************** */
   // item_*
   static boolean file(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "file")) return false;
@@ -67,57 +50,202 @@ public class ProofFrogParser implements PsiParser, LightPsiParser {
   /* ********************************************************** */
   // module_import |
   //     module_export |
-  //     datatype |
-  //     LINE_COMMENT
+  //     (P_SEMIC|P_COLON|P_COMMA|P_PERIOD) |  // Punctuation
+  //     (S_BRACE_L|S_BRACE_R|S_BRACKET_L|S_BRACKET_R|S_PAREN_L|S_PAREN_R) |  // Structure
+  //     (
+  //         O_SMPL|O_ASGN|O_MULT|O_ADD|O_SUB|O_DIV|O_EQ|O_OPT|O_NOT|
+  //         O_NEQ|O_GT|O_GEQ|O_LEQ|O_LT|O_OR|O_AND|O_VBAR|O_BSLASH
+  //     ) |  // Operators
+  //     (
+  //         K_IMPORT|K_EXPORT|K_RETURN|K_SUBSETS|K_ORACLES|K_AGAINST|
+  //         K_COMPOSE|K_THEOREM|K_EXTENDS|K_REQUIRES|K_INDUCTION|K_ASSUME|
+  //         K_PROOF|K_GAMES|K_UNION|K_CALLS|K_FROM|K_ELSE|K_LET|K_FOR|
+  //         K_IF|K_TO|K_IN|K_AS|K_NONE
+  //     ) |  // Keywords
+  //     (F_ADVERSARY|F_PRIMITIVE|F_REDUCTION|F_SCHEME|F_PHASE|F_GAME) |  // Functions
+  //     (T_SET|T_BOOL|T_VOID|T_INT|T_MAP|T_ARRAY|T_BITSTRING) |  // Datatypes
+  //     (V_BOOL|V_INT|V_BINARYNUM|V_LINE_COMMENT|V_FILESTRING|V_IDENT)
   static boolean item_(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "item_")) return false;
     boolean r;
+    Marker m = enter_section_(b);
     r = module_import(b, l + 1);
     if (!r) r = module_export(b, l + 1);
-    if (!r) r = datatype(b, l + 1);
-    if (!r) r = consumeToken(b, LINE_COMMENT);
+    if (!r) r = item__2(b, l + 1);
+    if (!r) r = item__3(b, l + 1);
+    if (!r) r = item__4(b, l + 1);
+    if (!r) r = item__5(b, l + 1);
+    if (!r) r = item__6(b, l + 1);
+    if (!r) r = item__7(b, l + 1);
+    if (!r) r = item__8(b, l + 1);
+    exit_section_(b, m, null, r);
+    return r;
+  }
+
+  // P_SEMIC|P_COLON|P_COMMA|P_PERIOD
+  private static boolean item__2(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__2")) return false;
+    boolean r;
+    r = consumeToken(b, P_SEMIC);
+    if (!r) r = consumeToken(b, P_COLON);
+    if (!r) r = consumeToken(b, P_COMMA);
+    if (!r) r = consumeToken(b, P_PERIOD);
+    return r;
+  }
+
+  // S_BRACE_L|S_BRACE_R|S_BRACKET_L|S_BRACKET_R|S_PAREN_L|S_PAREN_R
+  private static boolean item__3(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__3")) return false;
+    boolean r;
+    r = consumeToken(b, S_BRACE_L);
+    if (!r) r = consumeToken(b, S_BRACE_R);
+    if (!r) r = consumeToken(b, S_BRACKET_L);
+    if (!r) r = consumeToken(b, S_BRACKET_R);
+    if (!r) r = consumeToken(b, S_PAREN_L);
+    if (!r) r = consumeToken(b, S_PAREN_R);
+    return r;
+  }
+
+  // O_SMPL|O_ASGN|O_MULT|O_ADD|O_SUB|O_DIV|O_EQ|O_OPT|O_NOT|
+  //         O_NEQ|O_GT|O_GEQ|O_LEQ|O_LT|O_OR|O_AND|O_VBAR|O_BSLASH
+  private static boolean item__4(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__4")) return false;
+    boolean r;
+    r = consumeToken(b, O_SMPL);
+    if (!r) r = consumeToken(b, O_ASGN);
+    if (!r) r = consumeToken(b, O_MULT);
+    if (!r) r = consumeToken(b, O_ADD);
+    if (!r) r = consumeToken(b, O_SUB);
+    if (!r) r = consumeToken(b, O_DIV);
+    if (!r) r = consumeToken(b, O_EQ);
+    if (!r) r = consumeToken(b, O_OPT);
+    if (!r) r = consumeToken(b, O_NOT);
+    if (!r) r = consumeToken(b, O_NEQ);
+    if (!r) r = consumeToken(b, O_GT);
+    if (!r) r = consumeToken(b, O_GEQ);
+    if (!r) r = consumeToken(b, O_LEQ);
+    if (!r) r = consumeToken(b, O_LT);
+    if (!r) r = consumeToken(b, O_OR);
+    if (!r) r = consumeToken(b, O_AND);
+    if (!r) r = consumeToken(b, O_VBAR);
+    if (!r) r = consumeToken(b, O_BSLASH);
+    return r;
+  }
+
+  // K_IMPORT|K_EXPORT|K_RETURN|K_SUBSETS|K_ORACLES|K_AGAINST|
+  //         K_COMPOSE|K_THEOREM|K_EXTENDS|K_REQUIRES|K_INDUCTION|K_ASSUME|
+  //         K_PROOF|K_GAMES|K_UNION|K_CALLS|K_FROM|K_ELSE|K_LET|K_FOR|
+  //         K_IF|K_TO|K_IN|K_AS|K_NONE
+  private static boolean item__5(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__5")) return false;
+    boolean r;
+    r = consumeToken(b, K_IMPORT);
+    if (!r) r = consumeToken(b, K_EXPORT);
+    if (!r) r = consumeToken(b, K_RETURN);
+    if (!r) r = consumeToken(b, K_SUBSETS);
+    if (!r) r = consumeToken(b, K_ORACLES);
+    if (!r) r = consumeToken(b, K_AGAINST);
+    if (!r) r = consumeToken(b, K_COMPOSE);
+    if (!r) r = consumeToken(b, K_THEOREM);
+    if (!r) r = consumeToken(b, K_EXTENDS);
+    if (!r) r = consumeToken(b, K_REQUIRES);
+    if (!r) r = consumeToken(b, K_INDUCTION);
+    if (!r) r = consumeToken(b, K_ASSUME);
+    if (!r) r = consumeToken(b, K_PROOF);
+    if (!r) r = consumeToken(b, K_GAMES);
+    if (!r) r = consumeToken(b, K_UNION);
+    if (!r) r = consumeToken(b, K_CALLS);
+    if (!r) r = consumeToken(b, K_FROM);
+    if (!r) r = consumeToken(b, K_ELSE);
+    if (!r) r = consumeToken(b, K_LET);
+    if (!r) r = consumeToken(b, K_FOR);
+    if (!r) r = consumeToken(b, K_IF);
+    if (!r) r = consumeToken(b, K_TO);
+    if (!r) r = consumeToken(b, K_IN);
+    if (!r) r = consumeToken(b, K_AS);
+    if (!r) r = consumeToken(b, K_NONE);
+    return r;
+  }
+
+  // F_ADVERSARY|F_PRIMITIVE|F_REDUCTION|F_SCHEME|F_PHASE|F_GAME
+  private static boolean item__6(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__6")) return false;
+    boolean r;
+    r = consumeToken(b, F_ADVERSARY);
+    if (!r) r = consumeToken(b, F_PRIMITIVE);
+    if (!r) r = consumeToken(b, F_REDUCTION);
+    if (!r) r = consumeToken(b, F_SCHEME);
+    if (!r) r = consumeToken(b, F_PHASE);
+    if (!r) r = consumeToken(b, F_GAME);
+    return r;
+  }
+
+  // T_SET|T_BOOL|T_VOID|T_INT|T_MAP|T_ARRAY|T_BITSTRING
+  private static boolean item__7(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__7")) return false;
+    boolean r;
+    r = consumeToken(b, T_SET);
+    if (!r) r = consumeToken(b, T_BOOL);
+    if (!r) r = consumeToken(b, T_VOID);
+    if (!r) r = consumeToken(b, T_INT);
+    if (!r) r = consumeToken(b, T_MAP);
+    if (!r) r = consumeToken(b, T_ARRAY);
+    if (!r) r = consumeToken(b, T_BITSTRING);
+    return r;
+  }
+
+  // V_BOOL|V_INT|V_BINARYNUM|V_LINE_COMMENT|V_FILESTRING|V_IDENT
+  private static boolean item__8(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item__8")) return false;
+    boolean r;
+    r = consumeToken(b, V_BOOL);
+    if (!r) r = consumeToken(b, V_INT);
+    if (!r) r = consumeToken(b, V_BINARYNUM);
+    if (!r) r = consumeToken(b, V_LINE_COMMENT);
+    if (!r) r = consumeToken(b, V_FILESTRING);
+    if (!r) r = consumeToken(b, V_IDENT);
     return r;
   }
 
   /* ********************************************************** */
-  // EXPORT AS ID SEMI
+  // K_EXPORT K_AS V_IDENT P_SEMI
   public static boolean module_export(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "module_export")) return false;
-    if (!nextTokenIs(b, EXPORT)) return false;
+    if (!nextTokenIs(b, K_EXPORT)) return false;
     boolean r;
     Marker m = enter_section_(b);
-    r = consumeTokens(b, 0, EXPORT, AS, ID, SEMI);
+    r = consumeTokens(b, 0, K_EXPORT, K_AS, V_IDENT, P_SEMI);
     exit_section_(b, m, MODULE_EXPORT, r);
     return r;
   }
 
   /* ********************************************************** */
-  // IMPORT FILESTRING (AS ID)? SEMI
+  // K_IMPORT V_FILESTRING (K_AS V_IDENT)? P_SEMI
   public static boolean module_import(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "module_import")) return false;
-    if (!nextTokenIs(b, IMPORT)) return false;
+    if (!nextTokenIs(b, K_IMPORT)) return false;
     boolean r;
     Marker m = enter_section_(b);
-    r = consumeTokens(b, 0, IMPORT, FILESTRING);
+    r = consumeTokens(b, 0, K_IMPORT, V_FILESTRING);
     r = r && module_import_2(b, l + 1);
-    r = r && consumeToken(b, SEMI);
+    r = r && consumeToken(b, P_SEMI);
     exit_section_(b, m, MODULE_IMPORT, r);
     return r;
   }
 
-  // (AS ID)?
+  // (K_AS V_IDENT)?
   private static boolean module_import_2(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "module_import_2")) return false;
     module_import_2_0(b, l + 1);
     return true;
   }
 
-  // AS ID
+  // K_AS V_IDENT
   private static boolean module_import_2_0(PsiBuilder b, int l) {
     if (!recursion_guard_(b, l, "module_import_2_0")) return false;
     boolean r;
     Marker m = enter_section_(b);
-    r = consumeTokens(b, 0, AS, ID);
+    r = consumeTokens(b, 0, K_AS, V_IDENT);
     exit_section_(b, m, null, r);
     return r;
   }
--- src/main/gen/io/github/aabmets/prooffroglang/psi/ProofFrogTypes.java
+++ src/main/gen/io/github/aabmets/prooffroglang/psi/ProofFrogTypes.java
@@ -8,32 +8,87 @@ import io.github.aabmets.prooffroglang.psi.impl.*;
 
 public interface ProofFrogTypes {
 
-  IElementType DATATYPE = new ProofFrogElementType("DATATYPE");
   IElementType MODULE_EXPORT = new ProofFrogElementType("MODULE_EXPORT");
   IElementType MODULE_IMPORT = new ProofFrogElementType("MODULE_IMPORT");
 
-  IElementType ARRAY = new ProofFrogTokenType("ARRAY");
-  IElementType AS = new ProofFrogTokenType("AS");
-  IElementType BITSTRING = new ProofFrogTokenType("BITSTRING");
-  IElementType BOOL = new ProofFrogTokenType("BOOL");
-  IElementType EXPORT = new ProofFrogTokenType("EXPORT");
-  IElementType FILESTRING = new ProofFrogTokenType("FILESTRING");
-  IElementType ID = new ProofFrogTokenType("ID");
-  IElementType IMPORT = new ProofFrogTokenType("IMPORT");
-  IElementType INT = new ProofFrogTokenType("INT");
-  IElementType LINE_COMMENT = new ProofFrogTokenType("LINE_COMMENT");
-  IElementType MAP = new ProofFrogTokenType("MAP");
-  IElementType SEMI = new ProofFrogTokenType("SEMI");
-  IElementType SET = new ProofFrogTokenType("SET");
-  IElementType VOID = new ProofFrogTokenType("VOID");
+  IElementType F_ADVERSARY = new ProofFrogTokenType("F_ADVERSARY");
+  IElementType F_GAME = new ProofFrogTokenType("F_GAME");
+  IElementType F_PHASE = new ProofFrogTokenType("F_PHASE");
+  IElementType F_PRIMITIVE = new ProofFrogTokenType("F_PRIMITIVE");
+  IElementType F_REDUCTION = new ProofFrogTokenType("F_REDUCTION");
+  IElementType F_SCHEME = new ProofFrogTokenType("F_SCHEME");
+  IElementType K_AGAINST = new ProofFrogTokenType("K_AGAINST");
+  IElementType K_AS = new ProofFrogTokenType("K_AS");
+  IElementType K_ASSUME = new ProofFrogTokenType("K_ASSUME");
+  IElementType K_CALLS = new ProofFrogTokenType("K_CALLS");
+  IElementType K_COMPOSE = new ProofFrogTokenType("K_COMPOSE");
+  IElementType K_ELSE = new ProofFrogTokenType("K_ELSE");
+  IElementType K_EXPORT = new ProofFrogTokenType("K_EXPORT");
+  IElementType K_EXTENDS = new ProofFrogTokenType("K_EXTENDS");
+  IElementType K_FOR = new ProofFrogTokenType("K_FOR");
+  IElementType K_FROM = new ProofFrogTokenType("K_FROM");
+  IElementType K_GAMES = new ProofFrogTokenType("K_GAMES");
+  IElementType K_IF = new ProofFrogTokenType("K_IF");
+  IElementType K_IMPORT = new ProofFrogTokenType("K_IMPORT");
+  IElementType K_IN = new ProofFrogTokenType("K_IN");
+  IElementType K_INDUCTION = new ProofFrogTokenType("K_INDUCTION");
+  IElementType K_LET = new ProofFrogTokenType("K_LET");
+  IElementType K_NONE = new ProofFrogTokenType("K_NONE");
+  IElementType K_ORACLES = new ProofFrogTokenType("K_ORACLES");
+  IElementType K_PROOF = new ProofFrogTokenType("K_PROOF");
+  IElementType K_REQUIRES = new ProofFrogTokenType("K_REQUIRES");
+  IElementType K_RETURN = new ProofFrogTokenType("K_RETURN");
+  IElementType K_SUBSETS = new ProofFrogTokenType("K_SUBSETS");
+  IElementType K_THEOREM = new ProofFrogTokenType("K_THEOREM");
+  IElementType K_TO = new ProofFrogTokenType("K_TO");
+  IElementType K_UNION = new ProofFrogTokenType("K_UNION");
+  IElementType O_ADD = new ProofFrogTokenType("O_ADD");
+  IElementType O_AND = new ProofFrogTokenType("O_AND");
+  IElementType O_ASGN = new ProofFrogTokenType("O_ASGN");
+  IElementType O_BSLASH = new ProofFrogTokenType("O_BSLASH");
+  IElementType O_DIV = new ProofFrogTokenType("O_DIV");
+  IElementType O_EQ = new ProofFrogTokenType("O_EQ");
+  IElementType O_GEQ = new ProofFrogTokenType("O_GEQ");
+  IElementType O_GT = new ProofFrogTokenType("O_GT");
+  IElementType O_LEQ = new ProofFrogTokenType("O_LEQ");
+  IElementType O_LT = new ProofFrogTokenType("O_LT");
+  IElementType O_MULT = new ProofFrogTokenType("O_MULT");
+  IElementType O_NEQ = new ProofFrogTokenType("O_NEQ");
+  IElementType O_NOT = new ProofFrogTokenType("O_NOT");
+  IElementType O_OPT = new ProofFrogTokenType("O_OPT");
+  IElementType O_OR = new ProofFrogTokenType("O_OR");
+  IElementType O_SMPL = new ProofFrogTokenType("O_SMPL");
+  IElementType O_SUB = new ProofFrogTokenType("O_SUB");
+  IElementType O_VBAR = new ProofFrogTokenType("O_VBAR");
+  IElementType P_COLON = new ProofFrogTokenType("P_COLON");
+  IElementType P_COMMA = new ProofFrogTokenType("P_COMMA");
+  IElementType P_PERIOD = new ProofFrogTokenType("P_PERIOD");
+  IElementType P_SEMI = new ProofFrogTokenType("P_SEMI");
+  IElementType P_SEMIC = new ProofFrogTokenType("P_SEMIC");
+  IElementType S_BRACE_L = new ProofFrogTokenType("S_BRACE_L");
+  IElementType S_BRACE_R = new ProofFrogTokenType("S_BRACE_R");
+  IElementType S_BRACKET_L = new ProofFrogTokenType("S_BRACKET_L");
+  IElementType S_BRACKET_R = new ProofFrogTokenType("S_BRACKET_R");
+  IElementType S_PAREN_L = new ProofFrogTokenType("S_PAREN_L");
+  IElementType S_PAREN_R = new ProofFrogTokenType("S_PAREN_R");
+  IElementType T_ARRAY = new ProofFrogTokenType("T_ARRAY");
+  IElementType T_BITSTRING = new ProofFrogTokenType("T_BITSTRING");
+  IElementType T_BOOL = new ProofFrogTokenType("T_BOOL");
+  IElementType T_INT = new ProofFrogTokenType("T_INT");
+  IElementType T_MAP = new ProofFrogTokenType("T_MAP");
+  IElementType T_SET = new ProofFrogTokenType("T_SET");
+  IElementType T_VOID = new ProofFrogTokenType("T_VOID");
+  IElementType V_BINARYNUM = new ProofFrogTokenType("V_BINARYNUM");
+  IElementType V_BOOL = new ProofFrogTokenType("V_BOOL");
+  IElementType V_FILESTRING = new ProofFrogTokenType("V_FILESTRING");
+  IElementType V_IDENT = new ProofFrogTokenType("V_IDENT");
+  IElementType V_INT = new ProofFrogTokenType("V_INT");
+  IElementType V_LINE_COMMENT = new ProofFrogTokenType("V_LINE_COMMENT");
 
   class Factory {
     public static PsiElement createElement(ASTNode node) {
       IElementType type = node.getElementType();
-      if (type == DATATYPE) {
-        return new ProofFrogDatatypeImpl(node);
-      }
-      else if (type == MODULE_EXPORT) {
+      if (type == MODULE_EXPORT) {
         return new ProofFrogModuleExportImpl(node);
       }
       else if (type == MODULE_IMPORT) {
