9d080a5235c5982a1f7664982f5117b23c6816bf
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-05-12 21:18:56 +0300

    Updated bundle-publish.yml GitHub Action WIP

---
+++ scripts/create_bundle.py
@@ -0,0 +1,36 @@
+import os
+import tomllib
+import subprocess
+from packaging import tags
+
+
+def main():
+    with open('../proof_frog/pyproject.toml', 'rb') as f:
+        project = tomllib.load(f)['project']
+        proj_name = project['name']
+        proj_version = project['version']
+
+    first_tag = list(tags.sys_tags())[0]
+    platform = first_tag.platform
+
+    if 'GITHUB_ENV' in os.environ:
+        with open(os.environ['GITHUB_ENV'], 'a') as f:
+            artifact_name = f"{proj_name}-{proj_version}-{platform}"
+            f.write(f"artifact_name={artifact_name}\n")
+
+    subprocess.run([
+        'pyinstaller',
+        '--onedir',
+        '--paths', '.',
+        '--collect-all', 'antlr4_python3_runtime',
+        '--collect-all', 'colorama',
+        '--collect-all', 'sympy',
+        '--collect-all', 'z3_solver',
+        '--collect-all', 'z3',
+        '--name', proj_name,
+        'bundle_wrapper.py'
+    ], check=True)
+
+
+if __name__ == "__main__":
+    main()
