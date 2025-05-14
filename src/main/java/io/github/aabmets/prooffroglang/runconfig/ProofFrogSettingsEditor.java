1b33376f56ba640a29ff07472e27c56d5fdf07fe
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-05-14 22:37:40 +0300

    Run config file chooser root is now the project dir

--- src/main/java/io/github/aabmets/prooffroglang/runconfig/ProofFrogSettingsEditor.java
+++ src/main/java/io/github/aabmets/prooffroglang/runconfig/ProofFrogSettingsEditor.java
@@ -5,6 +5,8 @@ import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -19,6 +21,15 @@ public class ProofFrogSettingsEditor extends SettingsEditor<ProofFrogRunConfigur
         panel = new JPanel(new BorderLayout(5,5));
         filePathField = new TextFieldWithBrowseButton();
 
+        FileChooserDescriptor chooser = getFileChooserDescriptor(project);
+
+        filePathField.addBrowseFolderListener(new TextBrowseFolderListener(chooser, project));
+
+        panel.add(new JLabel("ProofFrog file:"), BorderLayout.WEST);
+        panel.add(filePathField, BorderLayout.CENTER);
+    }
+
+    private static @NotNull FileChooserDescriptor getFileChooserDescriptor(@NotNull Project project) {
         FileChooserDescriptor chooser = new FileChooserDescriptor(
                 true,  false, false, false, false, false
         );
@@ -27,11 +38,14 @@ public class ProofFrogSettingsEditor extends SettingsEditor<ProofFrogRunConfigur
             String ext = file.getExtension();
             return ext != null && List.of("primitive","scheme","game","proof").contains(ext);
         });
-
-        filePathField.addBrowseFolderListener(new TextBrowseFolderListener(chooser, project));
-
-        panel.add(new JLabel("ProofFrog file:"), BorderLayout.WEST);
-        panel.add(filePathField, BorderLayout.CENTER);
+        String basePath = project.getBasePath();
+        if (basePath != null) {
+            VirtualFile projectRoot = LocalFileSystem.getInstance().findFileByPath(basePath);
+            if (projectRoot != null) {
+                chooser.withRoots(projectRoot);
+            }
+        }
+        return chooser;
     }
 
     @Override
