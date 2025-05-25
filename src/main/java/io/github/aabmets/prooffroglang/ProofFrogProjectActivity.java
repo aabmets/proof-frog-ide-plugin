83289cfb19e0fad2eece39e910b86c1e47d5bda8
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-05-25 16:27:53 +0300

    Upgraded setup message during plugin startup

--- src/main/java/io/github/aabmets/prooffroglang/ProofFrogProjectActivity.java
+++ src/main/java/io/github/aabmets/prooffroglang/ProofFrogProjectActivity.java
@@ -3,8 +3,11 @@ package io.github.aabmets.prooffroglang;
 import io.github.aabmets.prooffroglang.actions.ProofFrogActionManager;
 import io.github.aabmets.prooffroglang.utils.*;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.ProjectActivity;
+import com.intellij.openapi.ui.Messages;
 import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
@@ -16,27 +19,46 @@ public class ProofFrogProjectActivity implements ProjectActivity {
     public @Nullable Object execute(
             @NotNull Project project,
             @NotNull Continuation<? super Unit> continuation) {
+        ProofFrogNotifier notifier = new ProofFrogNotifier(project);
 
-        if (!ProofFrogPaths.pythonVenvExists(project)) {
-            ProofFrogNotifier notifier = new ProofFrogNotifier(project);
-
-            try {
-                notifier.notifyInfo("Installing ProofFrog library...");
-                new ProofFrogSetup().runSetup();
-                notifier.notifyInfo(
-                    "ProofFrog installation successful",
-                    "Context menu actions and run configurations are now available."
-                );
-            } catch (RuntimeException e) {
-                notifier.notifyError(
-                    "ProofFrog installation failed",
-                    "You must manually install the ProofFrog library to" +
-                    "enable context menu actions and run configurations."
-                );
-            }
-        }
         if (ProofFrogPaths.pythonVenvExists(project)) {
             ProofFrogActionManager.registerContextMenu();
+            notifier.notifyInfo("ProofFrog library detected");
+        } else {
+            Application app = ApplicationManager.getApplication();
+            app.invokeLater(() -> {
+                int result = Messages.showYesNoDialog(
+                    project,
+                    """
+                            Would you like the plugin to automatically install \
+                            the ProofFrog library (requires internet connection)?
+                            
+                            The library is required to enable context menu actions
+                            and run configurations for ProofFrog files.
+                            """,
+                    "ProofFrog Library Not Detected",
+                    Messages.getQuestionIcon()
+                );
+                if (result == Messages.YES) {
+                    app.executeOnPooledThread(() -> {
+                        try {
+                            notifier.notifyInfo("Installing ProofFrog library...");
+                            new ProofFrogSetup().runSetup();
+                            ProofFrogActionManager.registerContextMenu();
+                            notifier.notifyInfo(
+                                "ProofFrog installation successful",
+                                "Context menu actions and run configurations are now available."
+                            );
+                        } catch (RuntimeException e) {
+                            notifier.notifyError(
+                                "ProofFrog installation failed",
+                                "You must manually install the ProofFrog library to" +
+                                "enable context menu actions and run configurations."
+                            );
+                        }
+                    });
+                }
+            });
         }
         return Unit.INSTANCE;
     }
